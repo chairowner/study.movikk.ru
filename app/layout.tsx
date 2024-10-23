173cdff96ac86b359002a0929c4708e6292f74ac
Danil Sagajdachnyj <chairowner@yandex.ru>
2024-10-23 08:15:53 +0500

    add opengraph meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,14 @@ import type { Metadata } from "next";
 import { YandexMetrika } from "./components/YandexMetrika/YandexMetrika";
 import "./styles/globals.scss";
 
+const url: string = process.env.PUBLIC_SITE_URL || "https://study.movikk.ru";
+const title: string = "Учебный центр «Мовикк»";
+const description: string =
+	"Аккредитация ваших курсов. Хотите выдавать документы государственного образца вашим ученикам? Наши специалисты проконсультируют вас по всем вопросам!";
+
 export const metadata: Metadata = {
-	title: "Учебный центр «Мовикк»",
-	description:
-		"Аккредитация ваших курсов. Хотите выдавать документы государственного образца вашим ученикам?",
+	title,
+	description,
 	keywords: [
 		"аккредитация",
 		"сайт аккредитации",
@@ -17,6 +21,22 @@ export const metadata: Metadata = {
 		"мовикк",
 		"movikk",
 	],
+	openGraph: {
+		title,
+		description,
+		url,
+		type: "website",
+		locale: "ru_RU",
+		siteName: title,
+		images: [
+			{
+				url: url + "/images/36e63456-61f3-46b7-b7f0-51b2c3c3924a.webp",
+				width: 1893,
+				height: 903,
+				alt: title,
+			},
+		],
+	},
 };
 
 export default function RootLayout({
